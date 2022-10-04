9fffeb232d2a945fe26db5dba94965b1537b3d0b
Kaue Pereira <kauefontes@outlook.com>
2022-10-03 21:57:32 -0400

    Compose fundamental concepts

---
+++ app/src/main/java/dev/quewui/dynamiccontentexample/MainActivity.kt
@@ -0,0 +1,59 @@
+package dev.quewui.dynamiccontentexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.quewui.dynamiccontentexample.ui.theme.DynamicContentExampleTheme
+
+val namesList: ArrayList<String> = arrayListOf("John", "Michael", "Andrew", "Danna", "Georgia")
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreetingsList(names = namesList)
+        }
+    }
+}
+
+@Composable
+fun GreetingsList(names: ArrayList<String>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+        Button(onClick = { namesList.add("Quewui") }) {
+            Text(text = "Add new Name")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+        style = MaterialTheme.typography.h4,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DynamicContentExampleTheme {
+        GreetingsList(names = namesList)
+    }
+}
